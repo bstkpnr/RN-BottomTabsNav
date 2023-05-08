524373b1a9bdb91e54fed4f6df8fcb600dd034c6
bstkpnr <bstkpnr01@gmail.com>
2023-05-08 16:28:18 +0300

    create customicon

---
+++ src/components/Icon/CustomIcon.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Text } from 'react-native';
+import AvesomeIcon  from 'react-native-vector-icons/FontAwesome5';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+
+type IconProps = {
+    iconSet: 'FontAwesome5' | 'MaterialCommunityIcons';
+    name: string;
+    size?: number;
+    color?: string;
+    style?: object;
+  };
+
+  const CustomIcon: React.FC<IconProps> = ({
+    iconSet,
+    name,
+    size = 24,
+    color = '#000',
+    style,
+  }) => {
+    switch (iconSet) {
+      case 'FontAwesome5':
+        return <AvesomeIcon name={name} size={size} color={color} style={style} />;
+      case 'MaterialCommunityIcons':
+        return <MaterialCommunityIcons name={name} size={size} color={color} style={style} />;
+      default:
+        return null;
+    }
+  };
+
+
+
+export default CustomIcon;
