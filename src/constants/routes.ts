078258e83b4feaf97eb23a9fc46bd18873b59900
bstkpnr <bstkpnr01@gmail.com>
2023-05-08 16:27:59 +0300

    route set

---
+++ src/constants/routes.ts
@@ -0,0 +1,12 @@
+const routes = {
+    HOME: 'Home',
+    READ: 'Read',
+    ABOUT: 'About',
+    GALLERY: 'Gallery',
+    LINKS: 'Links',
+    LISTEN: 'Listen',
+    MAIN:'Main'
+  };
+  
+  export default routes;
+  
