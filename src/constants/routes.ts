0c19666a6537cb299bed11bbe48963cdcde23042
bstkpnr <bstkpnr01@gmail.com>
2023-05-09 14:25:12 +0300

    edit fix

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,12 +1,11 @@
 const routes = {
-    HOME: 'Home',
-    READ: 'Read',
-    ABOUT: 'About',
-    GALLERY: 'Gallery',
-    LINKS: 'Links',
-    LISTEN: 'Listen',
-    MAIN:'Main'
-  };
-  
-  export default routes;
-  
+  HOME: 'Home',
+  READ: 'Read',
+  ABOUT: 'About',
+  GALLERY: 'Gallery',
+  LINKS: 'Links',
+  LISTEN: 'Listen',
+  MAIN: 'Main',
+};
+
+export default routes;
