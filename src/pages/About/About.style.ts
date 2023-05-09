ef2e034c8d6f7887fd1f74e5cb6442a290d1e4b8
bstkpnr <bstkpnr01@gmail.com>
2023-05-09 14:24:54 +0300

    edit fix

--- src/pages/About/About.style.ts
+++ src/pages/About/About.style.ts
@@ -1,17 +1,13 @@
-import { StyleSheet,Dimensions} from "react-native";
-const deviceSize=Dimensions.get('window');
+import {StyleSheet, Dimensions} from 'react-native';
 
 export default StyleSheet.create({
-container:{
-    flex:1,
-    backgroundColor:'#8294C4'
-
-},
-text:{
-    color:'white',
-    fontSize:24,
-    textAlign:'center'
-
-}
-   
-})
+  container: {
+    flex: 1,
+    backgroundColor: '#8294C4',
+  },
+  text: {
+    color: 'white',
+    fontSize: 24,
+    textAlign: 'center',
+  },
+});
--- src/pages/About/AboutPage.tsx
+++ src/pages/About/AboutPage.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Text, View } from 'react-native';
-import styles from './About.style'
+import {Text, View} from 'react-native';
+import styles from './About.style';
 
 const AboutPage: React.FC = () => {
   return (
