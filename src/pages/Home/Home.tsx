888869fd5f19e5683c575d9906eb07516ead32e5
bstkpnr <bstkpnr01@gmail.com>
2023-05-09 14:24:40 +0300

    edit fix

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import {Image, ImageBackground, Text, View} from 'react-native';
-import Icon from 'react-native-vector-icons/AntDesign';
-import styles from './Home.style'
+import {Image, View} from 'react-native';
+import styles from './Home.style';
 
 const Home: React.FC = () => {
   return (
     <View style={styles.container}>
-<Image source={require('../../assets/img/vangogh.jpg')} style={{width:400,height:'100%'}} />    
-</View>
+      <Image
+        source={require('../../assets/img/vangogh.jpg')}
+        style={{width: 400, height: '100%'}}
+      />
+    </View>
   );
 };
 
