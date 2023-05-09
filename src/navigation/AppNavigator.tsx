13472fb18a19b71e41e24edd5ad39fbe355ae6c0
bstkpnr <bstkpnr01@gmail.com>
2023-05-09 14:25:03 +0300

    edit fix

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,23 +1,17 @@
-import React from "react";
-import { NavigationContainer, RouteProp } from "@react-navigation/native";
-import { StackNavigationOptions, createStackNavigator } from "@react-navigation/stack";
-import ROUTES from "../constants/routes";
-import ReadPage from "../pages/Read/ReadPage";
-import ListenPage from "../pages/Listen/ListenPage";
-import GalleryPage from "../pages/Gallery/GalleryPage";
-import AboutPage from "../pages/About/AboutPage";
-import LinkPage from "../pages/Links/LinkPage";
-import BottomTabNavigator from "./BottomTabNavigator";
-import Home from "../pages/Home/Home";
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import ROUTES from '../constants/routes';
+
+import BottomTabNavigator from './BottomTabNavigator';
 
 const Stack = createStackNavigator();
 
 const AppNavigator: React.FC = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator  screenOptions={{ headerShown: false }}>
-      <Stack.Screen name={ROUTES.MAIN} component={BottomTabNavigator} />
-
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name={ROUTES.MAIN} component={BottomTabNavigator} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/navigation/BottomTabNavigator.tsx
+++ src/navigation/BottomTabNavigator.tsx
@@ -1,42 +1,55 @@
-import React from "react";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import Home from "../pages/Home/Home";
-import ReadPage from "../pages/Read/ReadPage";
-import ListenPage from "../pages/Listen/ListenPage";
-import GalleryPage from "../pages/Gallery/GalleryPage";
-import AboutPage from "../pages/About/AboutPage";
-import LinkPage from "../pages/Links/LinkPage";
-import ROUTES from "../constants/routes";
-import CustomIcon from "../components/Icon/CustomIcon";
+import React from 'react';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Home from '../pages/Home/Home';
+import ReadPage from '../pages/Read/ReadPage';
+import ListenPage from '../pages/Listen/ListenPage';
+import GalleryPage from '../pages/Gallery/GalleryPage';
+import AboutPage from '../pages/About/AboutPage';
+import LinkPage from '../pages/Links/LinkPage';
+import ROUTES from '../constants/routes';
+import CustomIcon from '../components/Icon/CustomIcon';
 
 const Tab = createBottomTabNavigator();
 
 const BottomTabNavigator: React.FC = () => {
   return (
-    <Tab.Navigator initialRouteName={ROUTES.HOME} screenOptions={({ route }) => ({
-      tabBarIcon: ({ color, size }) => {
-        let iconName;
+    <Tab.Navigator
+      initialRouteName={ROUTES.HOME}
+      screenOptions={({route}) => ({
+        tabBarIcon: ({color, size}) => {
+          let iconName;
 
-        if (route.name === ROUTES.READ) {
-          iconName = 'book-reader';
-        } else if (route.name === ROUTES.LISTEN) {
-          iconName = 'headphones';
-        } else if (route.name === ROUTES.GALLERY) {
-          iconName = 'photo-video';
-        } else if (route.name === ROUTES.ABOUT) {
-          iconName = 'info';
-        } else if (route.name === ROUTES.LINKS) {
-          iconName = 'link';
-        }
+          if (route.name === ROUTES.READ) {
+            iconName = 'book-reader';
+          } else if (route.name === ROUTES.LISTEN) {
+            iconName = 'headphones';
+          } else if (route.name === ROUTES.GALLERY) {
+            iconName = 'photo-video';
+          } else if (route.name === ROUTES.ABOUT) {
+            iconName = 'info';
+          } else if (route.name === ROUTES.LINKS) {
+            iconName = 'link';
+          }
 
-        return <CustomIcon name={iconName} size={size} color={color} iconSet='FontAwesome5' />;
-      },
-      tabBarActiveTintColor:'#009FBD',
-      tabBarInactiveTintColor: '#393646',
-      headerShown:false
-    })}>
-       <Tab.Screen name={ROUTES.HOME} component={Home} options={{ tabBarButton: () => null }}  />
-      <Tab.Screen name={ROUTES.READ} component={ReadPage}  />
+          return (
+            <CustomIcon
+              name={iconName}
+              size={size}
+              color={color}
+              iconSet="FontAwesome5"
+            />
+          );
+        },
+        tabBarActiveTintColor: '#009FBD',
+        tabBarInactiveTintColor: '#393646',
+        headerShown: false,
+      })}>
+      <Tab.Screen
+        name={ROUTES.HOME}
+        component={Home}
+        options={{tabBarButton: () => null}}
+      />
+      <Tab.Screen name={ROUTES.READ} component={ReadPage} />
       <Tab.Screen name={ROUTES.LISTEN} component={ListenPage} />
       <Tab.Screen name={ROUTES.GALLERY} component={GalleryPage} />
       <Tab.Screen name={ROUTES.ABOUT} component={AboutPage} />
