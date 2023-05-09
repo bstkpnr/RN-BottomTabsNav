3c05f1219bc8e9fca15ffc61ecea1b985b204676
bstkpnr <bstkpnr01@gmail.com>
2023-05-09 14:24:33 +0300

    edit fix

--- src/pages/Links/LinkPage.style.ts
+++ src/pages/Links/LinkPage.style.ts
@@ -1,18 +1,13 @@
-import { StyleSheet,Dimensions} from "react-native";
-const deviceSize=Dimensions.get('window');
+import {StyleSheet} from 'react-native';
 
 export default StyleSheet.create({
-
-    container:{
-        flex:1,
-        backgroundColor:'#6C9BCF'
-    
-    },
-    text:{
-        color:'white',
-        fontSize:24,
-        textAlign:'center'
-
-    
-    }
-})
+  container: {
+    flex: 1,
+    backgroundColor: '#6C9BCF',
+  },
+  text: {
+    color: 'white',
+    fontSize: 24,
+    textAlign: 'center',
+  },
+});
