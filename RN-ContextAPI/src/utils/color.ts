7457af035eeb58eeadd069292deafae1b9bec3a6
bstkpnr <bstkpnr01@gmail.com>
2023-05-24 10:32:16 +0300

    create project colors

---
+++ RN-ContextAPI/src/utils/color.ts
@@ -0,0 +1,10 @@
+const Colors = {
+  defaultWhiteColor: '#FFFFFF',
+  defaultBrownColor: 'rgba(67, 54, 42, 1)',
+  defaultGreyColor: '#252525',
+  defaultBlueColor: '#146C94',
+  defaultRedColor: '#E74646',
+  secondWhiteColor: '#F9F9F9',
+};
+
+export default Colors;
