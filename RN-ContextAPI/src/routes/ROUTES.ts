b0b4ff1b9912b0b7b87bcf774e620dfc743308ee
bstkpnr <bstkpnr01@gmail.com>
2023-05-24 10:49:14 +0300

    edit fix

---
+++ RN-ContextAPI/src/routes/ROUTES.ts
@@ -0,0 +1,11 @@
+const routes = {
+    HOME: 'Anasayfa',
+    PRODUCTLIST: 'Ürün Listesi',
+    BASKET: 'Sepet',
+    PRODUCTDETAIL: 'Ürün Detay',
+    BUYPRODUCT: 'Ürün Satın Al',
+
+  };
+  
+  export default routes;
+  
