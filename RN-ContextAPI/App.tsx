1ff5334c91b903f49f6eca046357de55ff51adf7
bstkpnr <bstkpnr01@gmail.com>
2023-05-28 23:07:19 +0300

    edit fix

--- RN-ContextAPI/App.tsx
+++ RN-ContextAPI/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import AppNavigator from './src/navigation/AppNavigator';
+import 'react-native-gesture-handler';
+
 
 function App(): JSX.Element {
   return <AppNavigator />;
