051e08d768ea35ca8edda734e27b9a104dae6c85
bstkpnr <bstkpnr01@gmail.com>
2023-05-28 23:09:09 +0300

    edit ifx

--- RN-ContextAPI/src/page/Home/Home.style.ts
+++ RN-ContextAPI/src/page/Home/Home.style.ts
@@ -1,13 +1,38 @@
 import {StyleSheet} from 'react-native';
+import Colors from '../../utils/color';
 
 export default StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#917FB3',
+    flex:1, flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+
+   
   },
   text: {
-    color: 'white',
-    fontSize: 24,
+    color:Colors.defaultBlackColor,
+    fontSize: 53,
     textAlign: 'center',
+    fontFamily:'Roboto-Regular',
+    fontWeight:'bold',
+    top:284
   },
+  imgbackground:{
+    width:'100%',height:'100%',
+    flex:1,
+    backgroundColor: Colors.defaultWhiteColor,
+    opacity: 0.55,
+  },
+  productTextContainer:{
+    display:'flex',
+    flexDirection:'row',
+    top:640,
+    justifyContent:'center'
+  },
+  productText:{
+    fontSize:16,
+    color:Colors.defaultBlackColor,
+    fontFamily:'Roboto-Regular',
+    fontWeight:'bold'
+  }
 });
--- RN-ContextAPI/src/page/Home/Home.tsx
+++ RN-ContextAPI/src/page/Home/Home.tsx
@@ -1,11 +1,33 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {ImageBackground, Text, View,TouchableOpacity} from 'react-native';
 import styles from './Home.style';
+import CustomIcon from '../../components/Icon/CustomIcon';
+import Colors from '../../utils/color';
+import { Navigation } from '../../utils/types';
+import routes from '../../routes/ROUTES';
 
-const HomePage: React.FC = () => {
+
+type Props = {
+  navigation: Navigation;
+};
+
+const HomePage: React.FC <Props>= ({navigation}) => {
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>HomePage</Text>
+      <ImageBackground
+        source={require('../../img/styling2.jpg')}
+        style={styles.imgbackground}>
+
+        <Text style={styles.text}>BESTCOM</Text>
+<TouchableOpacity onPress={()=>navigation.navigate(routes.PRODUCTLIST)}>
+        <View style={styles.productTextContainer}>
+  
+      <CustomIcon name='arrow-right' iconSet='FontAwesome5' size={16} />
+      <Text style={styles.productText}>Ürünleri Gör</Text>
+        </View>
+</TouchableOpacity>
+      </ImageBackground>
     </View>
   );
 };
